9c8f2cf06e77ab1b71dd4adaa4318dacecf179a6
João Victor Ferreira da Silva <jvictorfsilva@gmail.com>
2022-03-22 21:19:58 -0300

    Initial commit

---
+++ Game/main.py
@@ -0,0 +1,37 @@
+import pygame
+import sys
+from level import Level
+
+from settings import *
+
+
+# from debug import debug
+
+
+class Game:
+    def __init__(self):
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('NOME DO JOGO')
+        self.clock = pygame.time.Clock()
+
+        self.level = Level()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            self.level.run()
+            # debug('Test')
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
